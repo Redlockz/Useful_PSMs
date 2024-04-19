de918096ab48fb0d106c0d91242d0abb8e6964ff
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-19 21:35:53 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -1,5 +1,5 @@
 BeforeAll {
-    Import-Module -Name Update-Repositories -Force
+    Import-Module -Name /home/runner/work/Useful_PSMs/Update-Repositories/Update-Repositories.psm1 -Verbose -Force
 }
 
 Describe "Update-Repositories" {
