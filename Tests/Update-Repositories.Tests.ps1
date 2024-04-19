4c569998f56c1f8972354e9cca6459660d14772b
Jordi van Deerse <jjp.van.deerse@tvmoebius.onmicrosoft.com>
2024-04-19 21:02:07 +0200

    added Pester

---
+++ Tests/Update-Repositories.Tests.ps1
@@ -0,0 +1,10 @@
+BeforeAll {
+    Import-Module ..\Update-Repositories\Update-Repositories.psm1 -Force
+}
+
+Describe "Update-Repositories" {
+    # Import-Module -Name Update-Repositories -Force
+    It "Returns nothing to commit, working tree clean" {
+        Update-Repositories | Should -BeLike "On branch main Your branch is up to date with 'origin/main'.  nothing to commit, working tree clean"
+    }
+}
