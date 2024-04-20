a34c2d1e6ca8455760241817418201980e5146c6
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-20 09:57:45 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -8,6 +8,6 @@ Describe "Update-Repositories" {
     #     Update-Repositories | Should -BeLike "On branch main Your branch is up to date with 'origin/main'.  nothing to commit, working tree clean"
     # }
     It "Returns RuntimeException: " {
-        Update-Repositories | Should -Throw
+        Update-Repositories | Should -BeLike "Enter the path to your base git directory containing your git repositories, example C:\Users\User1\Projects:"
     }
 }
