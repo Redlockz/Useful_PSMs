d12d7b96f02d982f9095a966c2d426e87d8e8f14
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-20 09:54:21 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -4,7 +4,10 @@ BeforeAll {
 
 Describe "Update-Repositories" {
     # Import-Module -Name Update-Repositories -Force
-    It "Returns nothing to commit, working tree clean" {
-        Update-Repositories | Should -BeLike "On branch main Your branch is up to date with 'origin/main'.  nothing to commit, working tree clean"
+    # It "Returns nothing to commit, working tree clean" {
+    #     Update-Repositories | Should -BeLike "On branch main Your branch is up to date with 'origin/main'.  nothing to commit, working tree clean"
+    # }
+    It "Returns RuntimeException: " {
+        Update-Repositories | Should -BeLike "RuntimeException: location not found, are you sure you entered the right directory?"
     }
 }
