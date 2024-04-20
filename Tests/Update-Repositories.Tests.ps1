74afeec6ee1c632d61b08573f11236583b791fda
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-20 10:43:37 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -3,11 +3,31 @@ BeforeAll {
 }
 
 Describe "Update-Repositories" {
-    # Import-Module -Name Update-Repositories -Force
-    # It "Returns nothing to commit, working tree clean" {
-    #     Update-Repositories | Should -BeLike "On branch main Your branch is up to date with 'origin/main'.  nothing to commit, working tree clean"
-    # }
-    It "Should invoke Read-Host" {
-        Update-Repositories | Should -Invoke -CommandName Read-Host
+    Context "Read-Host" {
+        Mock Read-Host {return "/home/runner/work/Useful_PSMs/Useful_PSMs"}
+        # It "Should invoke Read-Host" {
+        #     Update-Repositories | Should -Invoke -CommandName Read-Host
+        # }
     }
 }
+
+# Import-Module -Name "c:\LocationOfModules\Pester"
+
+# Function Test-Foo {
+#     $filePath = Read-Host "Tell me a file path"
+#     $filePath
+# }
+# Describe "Test-Foo" {
+#   Context "When something" {
+#         Mock Read-Host {return "c:\example"}
+
+#         $result = Test-Foo
+
+#         It "Returns correct result" { # should work
+#             $result | Should Be "c:\example"
+#         }
+#          It "Returns correct result" { # should not work
+#             $result | Should Be "SomeThingWrong"
+#         }
+#     }
+# }
