be5097d26170f573e9febe405c87d71e1b33fa79
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-23 17:12:27 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -11,7 +11,7 @@ Describe "Update-Repositories" {
     Context "Enter git directory and update repository" {
         Set-Content -Path "D:\a\Useful_PSMs\Useful_PSMs\.ado_gitfolder.txt" -Value 'nada'
         It "Should invoke Read-Host" {
-            {Update-Repositories} | Should -Invoke -CommandName Read-Host
+            {Update-Repositories} | Should -Invoke -CommandName "Read-Host"
         }
     }
 }
