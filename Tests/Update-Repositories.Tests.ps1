45f384f8ca72c45360fbe6de6c5d6f8bf23e1ffc
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-23 19:20:23 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -10,7 +10,7 @@ Describe "Update-Repositories" {
     }
     Context "Enter git directory and update repository" {
         It "Should throw git error" {
-            $content = Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs'
+            $content = Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'
             {Update-Repositories} | Should -BeLike "on branch main"
         }
     }
