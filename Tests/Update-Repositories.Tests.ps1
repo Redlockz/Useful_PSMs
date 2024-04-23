1955ad24b2886b5a1b7dcbf15f948c40f4b46368
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-23 20:37:53 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -13,7 +13,7 @@ Describe "Update-Repositories" {
             {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
         }
         It "Should throw" {
-            {Update-Repositories} | Should -Throw
+            {Update-Repositories} | Should -BeLike "main"
         }
     }
 }
