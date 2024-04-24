feaab584581ce75416dc426312c6db165d144b7d
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-24 19:53:23 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -2,16 +2,21 @@ BeforeAll {
     Import-Module -Name D:\a\Useful_PSMs\Useful_PSMs\Update-Repositories\Update-Repositories.psm1 -Verbose -Force
 }
 
-Describe "Update-Repositories" {
-    Context "Update-Repositories" {
+Describe "Update-Repositories failing" {
+    Context "Update-Repositories failing" {
         It "Should throw" {
             {Update-Repositories} | Should -Throw "*location not found*"
         }
+    }
+}
+
+Describe "Update-Repositories passing" {
+    Context "Update-Repositories passing" {
         It "Create Test File" {
             {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
         }
         It "Success" {
-            Mock -ModuleName Update-Repositories Write-Host {}
+            Mock -ModuleName Update-Repositories Update-Repositories {}
             # Assert-MockCalled Write-Host -Exactly 3 -Scope It
             Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "testing location" }
             Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "Updated all repositories" }
