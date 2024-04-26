44b7be2985de547ae7368a7021024f0e699ec797
Jordi van Deerse <jjp.van.deerse@tvmoebius.onmicrosoft.com>
2024-04-26 19:50:24 +0200

    new mock test

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -17,6 +17,9 @@ Describe "Update-Repositories failing" {
         }
         It "Succeeds, but not a git folder" {
             {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
+            Write-Host dir
+            Write-Host dir D:\a\Useful_PSMs
+            Write-Host dir D:\a\Useful_PSMs\Useful_PSMs
             Remove-Item -Path D:\a\Useful_PSMs\Useful_PSMs\.git -Force
             Mock Test-Path { return $true }
             Mock Get-Content { return '$HOME\.ado_gitfolder.txt' }
