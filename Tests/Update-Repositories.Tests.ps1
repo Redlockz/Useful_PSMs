b474849b7348bdc94bc716aa209444392d7cc677
Jordi van Deerse <jjp.van.deerse@tvmoebius.onmicrosoft.com>
2024-04-26 19:35:31 +0200

    new mock test

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -16,7 +16,14 @@ Describe "Update-Repositories failing" {
             Mock Get-Content { return 'C:\Users\deerj00\.ado_gitfolder.txt' }
             Mock Set-Location { 'C:\Users\deerj00\Projecten\Useful_PSMs' }
             {Update-Repositories} | Should -Not -Throw
-
+        }
+        It "Succeeds, but not a git folder" {
+            {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
+            {Remove-Item -Path "D:\a\Useful_PSMs\Useful_PSMs\.git"} | Should -Not -Throw
+            Mock Test-Path { return $true }
+            Mock Get-Content { return 'C:\Users\deerj00\.ado_gitfolder.txt' }
+            Mock Set-Location { 'C:\Users\deerj00\Projecten\Useful_PSMs' }
+            {Update-Repositories} | Should -Not -Throw
         }
     }
 }
