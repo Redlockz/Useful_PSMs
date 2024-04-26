2993bab5e0856bf100229a81c78717a4329f3671
Jordi van Deerse <jjp.van.deerse@tvmoebius.onmicrosoft.com>
2024-04-26 14:11:36 +0200

    changed pester test

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -1,25 +1,19 @@
-BeforeAll {
-    Import-Module -Name D:\a\Useful_PSMs\Useful_PSMs\Update-Repositories\Update-Repositories.psm1 -Verbose -Force
-}
-
 Describe "Update-Repositories failing" {
+
+    BeforeAll {
+        Import-Module -Name D:\a\Useful_PSMs\Useful_PSMs\Update-Repositories\Update-Repositories.psm1 -Verbose -Force
+    }
+
     Context "Update-Repositories failing" {
         It "Should throw" {
             {Update-Repositories} | Should -Throw "*location not found*"
         }
-    }
-}
 
-
-Describe "Update-Repositories passing" {
-    Context "Update-Repositories passing" {
-        BeforeAll {
-            Write-Verbose "Creating test file"
-            {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
-            Mock Write-Host -ModuleName Update-Repositories { return 'Updated git workspace' }
-        }
         It "Success" {
-            {Update-Repositories} | Should -Be 'Updated git workspace'
+            {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
+            Mock Write-Host
+            {Update-Repositories} | Should -Not -Throw
+            Should -Invoke Write-Host -Exactly 3
         }
     }
 }
