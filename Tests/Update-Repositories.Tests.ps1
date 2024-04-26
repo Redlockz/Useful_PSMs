4bd8d805c502093502cb7358c7fa8224c9881544
Redlockz <108271288+Redlockz@users.noreply.github.com>
2024-04-26 16:37:48 +0200

    Update Update-Repositories.Tests.ps1

--- Tests/Update-Repositories.Tests.ps1
+++ Tests/Update-Repositories.Tests.ps1
@@ -2,19 +2,19 @@ Describe "Update-Repositories failing" {
 
     BeforeAll {
         Import-Module -Name D:\a\Useful_PSMs\Useful_PSMs\Update-Repositories\Update-Repositories.psm1 -Verbose -Force
-        {Update-Repositories}
     }
 
     Context "Update-Repositories failing" {
         It "Should throw" {
-            Should -Throw "*location not found*"
+            {Update-Repositories} | Should -Throw "*location not found*"
         }
     }
     
     Context "Update-Repositories succeeding" {
         It "Success" {
-            Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'
-            Mock Write-Host
+            # Mock Get-Content { "C:/test/.ado_gitfolder.txt" }
+            {Set-Content -Path "$HOME\.ado_gitfolder.txt" -Value 'D:\a\Useful_PSMs\Useful_PSMs'} | Should -Not -Throw
+            {Update-Repositories} | Should -Not -Throw
             Should -Invoke Write-Host -Exactly 3
         }
     }
